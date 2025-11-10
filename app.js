7d83c8c7a451333d6e77b4ed85a821842df72bbb
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-11-11 01:22:17 +0400

    feat(homepage): setup messages and ejs

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = 3000;
 const app = express();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (_, res) => {
+  res.render("index", { messages: messages });
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Successfully ran server on port ${PORT}`);
 });
